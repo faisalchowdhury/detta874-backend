97e1aa697cc8bb46e15a92616890c5691189972e
faisalchowdhury <chowdhuryfaisal66@gmail.com>
2026-07-16 10:55:12 +0600

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   "https://dashboard-heirloom.netlify.app",
   "http://localhost:5173", // add your dev origin(s)
+  "http://187.124.231.5",
 ];
 app.use(
   cors({
